f8302db66a90ba2a52f549f9ace505556bbb9352
lintang digdoyo <digdoyo.lintang@gmail.com>
2023-02-20 13:20:17 +0700

    feat: add AnimeItem component

--- components/AnimeList/index.tsx
+++ components/AnimeList/index.tsx
@@ -3,28 +3,17 @@ import { SwiperSlide } from "swiper/react";
 import Carousel from "components/Common/Carousel";
 import styleList from "styles/components/AnimeList.module.scss";
 import styleCarousel from "styles/components/Carousel.module.scss";
+import AnimeItem from "./AnimeItem";
 
 const AnimeList = () => {
   return (
     <div className={styleList.list}>
       <Carousel>
         <SwiperSlide className={styleCarousel.carousel__item}>
-          <div>1</div>
+          <AnimeItem />
         </SwiperSlide>
         <SwiperSlide className={styleCarousel.carousel__item}>
-          <div>2</div>
-        </SwiperSlide>
-        <SwiperSlide className={styleCarousel.carousel__item}>
-          <div>3</div>
-        </SwiperSlide>
-        <SwiperSlide className={styleCarousel.carousel__item}>
-          <div>4</div>
-        </SwiperSlide>
-        <SwiperSlide className={styleCarousel.carousel__item}>
-          <div>5</div>
-        </SwiperSlide>
-        <SwiperSlide className={styleCarousel.carousel__item}>
-          <div>6</div>
+          <AnimeItem />
         </SwiperSlide>
       </Carousel>
     </div>
