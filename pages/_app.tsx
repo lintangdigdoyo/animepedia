251c5185471309c866943bb7b921f25060956404
lintang digdoyo <digdoyo.lintang@gmail.com>
2023-02-20 09:47:21 +0700

    init config react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from "next/app";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import "styles/base.scss";
 
+import queryClient from "services/queryClient";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
