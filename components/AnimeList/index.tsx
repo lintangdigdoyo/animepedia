d16650dd3e27bf9916bec40010d7f9dc18e8c494
lintang digdoyo <digdoyo.lintang@gmail.com>
2023-02-20 13:35:40 +0700

    feat: add input component

--- components/AnimeList/index.tsx
+++ components/AnimeList/index.tsx
@@ -1,13 +1,20 @@
 import { SwiperSlide } from "swiper/react";
 
 import Carousel from "components/Common/Carousel";
+import AnimeItem from "./AnimeItem";
 import styleList from "styles/components/AnimeList.module.scss";
 import styleCarousel from "styles/components/Carousel.module.scss";
-import AnimeItem from "./AnimeItem";
+import styleInput from "styles/components/Input.module.scss";
 
 const AnimeList = () => {
   return (
     <div className={styleList.list}>
+      <div className={styleList.list__header}>
+        <h4 className={styleList.list__title}>Anime List</h4>
+        <div className={styleList.list__search}>
+          <input className={styleInput.input} type="text" />
+        </div>
+      </div>
       <Carousel>
         <SwiperSlide className={styleCarousel.carousel__item}>
           <AnimeItem />
