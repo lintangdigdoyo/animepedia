7b2380fc92442c9785f77c789dac1a672e52def3
lintang digdoyo <digdoyo.lintang@gmail.com>
2023-02-21 18:17:39 +0700

    remove unsplash image config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["source.unsplash.com", "img.youtube.com", "cdn.myanimelist.net"],
+    domains: ["img.youtube.com", "cdn.myanimelist.net"],
   },
   env: {
     API_BASE_URL: process.env.API_BASE_URL || "",
